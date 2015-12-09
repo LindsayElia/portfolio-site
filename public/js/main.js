e184bc93c74ffa13806cb236e698199e62e19b73
LindsayElia <lindsay.elia@gmail.com>
2015-12-09 12:15:16 -0800

    positioning of logo, headshot, and text on home page fixd. show & hide of olark box on scroll for various page sizes added

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,39 @@
 console.log("file main.js is loaded");
 
 
-// show the chatbox when the page is scrolled
-$( window ).scroll(function() {
-  olark('api.box.show');
+
+// on initial page load
+// hide Olark on screen sizes 595px or smaller
+// show the Olark box on larger screens when page scrolls
+$(window).load(function() {
+	if ($(window).width() < 595) {
+		olark('api.box.hide');
+	} else {
+		$(window).scroll(function() {
+			olark('api.box.show');
+		});
+	}
 });
+
+// do same thing on window resize
+$(window).resize(function() {
+	var currentWidth = $(this).width();
+	if(currentWidth < 595 ) {
+		olark('api.box.hide');
+		// not sure why the box shows when the window is scrolled while smaller,
+		// but this fixes it
+		$(window).scroll(function() {
+			olark('api.box.hide');
+		});
+	} else {
+		$(window).scroll(function() {
+			olark('api.box.show');
+		});
+	}
+});
+	
+
+
 
 
 // use icon in footer to expand & minimize the olark chatbox
